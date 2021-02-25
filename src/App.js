4bec0b7bdd277c6ca73e730f8e57d905f8dc3109
Alibek-tse <tokanov.a.k@gmail.com>
2021-02-25 15:32:58 +0600

    add player

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Section from "./Component/Section";
+import Player from "./Component/Player"
 import "./App.css";
 
 function App() {
 
   return (
     <div>
+      <Player></Player>
       <Section></Section>
     </div>
   );
