b75fd7c2df289d4e220feb609dbba4d7775af848
Alibek-tse <tokanov.a.k@gmail.com>
2021-03-04 00:46:33 +0600

    add fullscreen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Section from "./Component/Section";
-import Player from "./Component/Player";
+// import Player from "./Component/Player";
 import "./App.css";
 import Footer from "./Component/Footer";
 import ButtonScreen from './Component/ButtonScreen'
@@ -15,7 +15,7 @@ function App() {
     <FullScreen handle={handle}>
       <div className="app">
         <ButtonScreen btnScreen={handle.enter}></ButtonScreen>
-        <Player></Player>
+        {/* <Player></Player> */}
         <Section fullscreen={handle.active}></Section>
         <Footer></Footer>
       </div>
